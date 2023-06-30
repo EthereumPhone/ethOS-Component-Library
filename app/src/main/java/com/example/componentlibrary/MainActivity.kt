35db99bd020ef220f3d598cc63dde42eb21a5c61
emunsi <emunsi123@outlook.com>
2023-06-30 16:17:34 +0200

    jdk change

--- app/src/main/java/com/example/componentlibrary/MainActivity.kt
+++ app/src/main/java/com/example/componentlibrary/MainActivity.kt
@@ -10,16 +10,28 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.SnackbarHostState
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.componentlibrary.ui.components.SnackbarState
+import com.example.componentlibrary.ui.components.ethOSButton
+import com.example.componentlibrary.ui.components.ethOSDropDownMenu
+import com.example.componentlibrary.ui.components.ethOSSnackbarHost
+import com.example.componentlibrary.ui.components.ethOSTextfield
+import com.example.componentlibrary.ui.components.rememberDropdownStateHolder
+import com.example.componentlibrary.ui.components.rememberSnackbarDelegate
 import com.example.componentlibrary.ui.theme.ethOSTheme
 
 import com.example.componentlibrary.ui.theme.error
+import kotlinx.coroutines.launch
+
 //Composables
 //import com.example.componentlibrary.ui.components.ethOSTextfield
 //import com.example.componentlibrary.ui.components.rememberSnackbarDelegate
@@ -52,7 +64,7 @@ fun GreetingPreview() {
 
         val scope = rememberCoroutineScope()
         val hostState = remember { SnackbarHostState() }
-        //val sdeg = rememberSnackbarDelegate(hostState,scope)
+        val sdeg = rememberSnackbarDelegate(hostState,scope)
 
         //Textfield
         Box() {
@@ -64,15 +76,15 @@ fun GreetingPreview() {
                 verticalArrangement = Arrangement.SpaceBetween
             ) {
                 Text(text = "Hallow")
-            //ethOSTextfield(value = "", placeholder = "Amount", modifier = Modifier.fillMaxWidth())
-            //ethOSTextfield(value = "", placeholder = "Amount", modifier = Modifier.fillMaxWidth())
-                /*ethOSButton(
+            ethOSTextfield(value = "", placeholder = "Amount", modifier = Modifier.fillMaxWidth())
+            ethOSTextfield(value = "", placeholder = "Amount", modifier = Modifier.fillMaxWidth())
+                ethOSButton(
                     "Mint",
                     R.drawable.baseline_swap_vert_24,
                     true,
                     onClick = {
                         sdeg.coroutineScope.launch {
-                            sdeg.showSnackbar(SnackbarState.WARNING,"DEFAULT")
+                            sdeg.showSnackbar(SnackbarState.SUCCESS,"DEFAULT")
                         }
                     },
                     primary = true)
@@ -82,9 +94,9 @@ fun GreetingPreview() {
                 val s = rememberDropdownStateHolder(items)
                 ethOSDropDownMenu(stateHolder = s)
             }
-            ethOSSnackbarHost(delegate = sdeg, modifier = Modifier.padding(12.dp).align(alignment = Alignment.BottomStart))*\
-                 */
-            }
+            ethOSSnackbarHost(delegate = sdeg, modifier = Modifier.padding(12.dp).align(alignment = Alignment.BottomStart))
+
+
         }
 
 
