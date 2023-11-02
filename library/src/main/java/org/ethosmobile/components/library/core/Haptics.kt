2b9c8a6f3812be6aecfe1940b97a2678faf73e03
emunsi <emunsi123@outlook.com>
2023-11-02 12:54:10 +0100

    change Haptics class

--- library/src/main/java/org/ethosmobile/components/library/core/Haptics.kt
+++ library/src/main/java/org/ethosmobile/components/library/core/Haptics.kt
@@ -1,15 +1,17 @@
 package org.ethosmobile.components.library.core
 
 import android.os.VibrationEffect
+import android.view.HapticFeedbackConstants
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+
 
 class Haptics {
 
     val POSITIVE_HAPTIC: VibrationEffect = VibrationEffect.createWaveform(
         longArrayOf(
-            25,25,25,25,25,200,25,25,25,25,25,25,25,25
+            25,25,25,25,25,250,25,25,25,25,25,25,25,25
         ),
         intArrayOf(
             0,20,40,60,80,100,80,60,50,40,30,20,10,0
@@ -17,18 +19,16 @@ class Haptics {
         -1
     )
 
-    val NEUTRAL_HAPTIC: VibrationEffect = VibrationEffect.startComposition().addPrimitive(
-        VibrationEffect.Composition.PRIMITIVE_SLOW_RISE, 1F,).compose()
+    //Use with View -> LocalView.current.performHapticFeedback(NEUTRAL_HAPTIC)
+    val NEUTRAL_HAPTIC: Int = HapticFeedbackConstants.CONFIRM
 
-
-    val NEGATIVE_HAPTIC: VibrationEffect = VibrationEffect.startComposition()
-        .addPrimitive(
-            VibrationEffect.Composition.PRIMITIVE_QUICK_FALL,//PRIMITIVE_CLICK,
-            0.5F,
-            50
-        ).addPrimitive(
-            VibrationEffect.Composition.PRIMITIVE_QUICK_FALL,//PRIMITIVE_CLICK,
-            0.7F,
-            100
-        ).compose()
+    val NEGATIVE_HAPTIC: VibrationEffect = VibrationEffect.createWaveform(
+        longArrayOf(
+            100,25,25, 100,25,25
+        ),
+        intArrayOf(
+            100,50,0, 100,50,0
+        ),
+        -1
+    )
 }
