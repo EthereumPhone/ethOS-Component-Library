74db56ecda216a76edba2b6a39106c9c212d284a
emunsi <emunsi123@outlook.com>
2023-12-18 02:45:54 +0100

    Networkpill

--- library/src/main/java/org/ethosmobile/components/library/core/Networkpill.kt
+++ library/src/main/java/org/ethosmobile/components/library/core/Networkpill.kt
@@ -1,11 +1,13 @@
 package org.ethosmobile.components.library.core
 
+import android.annotation.SuppressLint
 import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context
 import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -15,6 +17,7 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -22,83 +25,93 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 //import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import org.ethosmobile.components.library.theme.Colors
 
 @Composable
 fun ethOSNetworkPill(
+    modifier: Modifier=Modifier,
     address: String,
-    chainId: Int,
+    network: String,
+    chainColor: Color,
 ) {
 
 
-    var network = ""
-    when (chainId) {
-        1 -> network = "Mainnet"
-        5 -> network = "Goerli"
-        10 -> network = "Optimism"
-        137 -> network = "Polygon"
-        42161 -> network = "Arbitrum"
-        else -> {
-            network = "Mainnet"
-        }
-    }
-
-    var color = Color(0xFF2E3D4E)
-    when (chainId) {
-        1 -> color = Color(0xFF45BA4A) //Mainnet
-        5 -> color = Color(0xFFFCA311)
-        10 -> color = Color(0xFFE52222)
-        137 -> color = Color(0xFF8A50DA) //Goerli
-        42161 -> color = Color(0xFF49A9F2)
-        else -> {
-            color = Color(0xFF45BA4A)
-        }
-    }
-
-    val text = network+" " + address.take(5) + "..."
     val context = LocalContext.current
-    Row(
-        verticalAlignment = Alignment.CenterVertically,
-        modifier = Modifier
-            .clip(RoundedCornerShape(50.dp))
-            .background(Color(0xFF2E3D4E))
-            .padding(vertical = 6.dp, horizontal = 12.dp)
-            .clickable {
-                val clipboard: ClipboardManager? =
-                    context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager?
-                val clip = ClipData.newPlainText("Address", address)
-                clipboard?.setPrimaryClip(clip)
-
-                // Toast copy to clipboard
-                Toast
-                    .makeText(context, "Copied to clipboard", Toast.LENGTH_SHORT)
-                    .show()
-
-            }
+    Surface (
+        modifier = modifier.clip(CircleShape).clickable {
+            copyTextToClipboard(context,address)
+        },
+        color = Colors.DARK_GRAY,
+        contentColor = Color.White
     ) {
-        Box (
-            modifier = Modifier
-                .clip(CircleShape)
-                .background(color)
-                .size(15.dp)
-        ) {}
-        Spacer(modifier = Modifier.width(8.dp))
-        Text(
-            text = text,
-            fontWeight = FontWeight.Normal,
-            style = MaterialTheme.typography.button,
-            color = Color.White
-        )
+        Row (
+            horizontalArrangement = Arrangement.spacedBy(12.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = modifier.padding(4.dp,4.dp)//(4.dp,4.dp)
+        ){
+            //Add later
+            Box(
+                modifier = modifier.clip(CircleShape)
+                    .size(24.dp)
+                    .background(chainColor)
+
+            )
+            Text(
+                text = network,
+                fontSize = 16.sp,
+                fontWeight = FontWeight.SemiBold,
+                color = Color.White
+            )
+            Text(
+                modifier = modifier.padding(end=12.dp),
+                text = truncateText(address),
+                fontSize = 16.sp,
+                fontWeight = FontWeight.SemiBold,
+                color = Color.White
+            )
+        }
+
     }
 }
 
+private fun chainName(chainId: String) = when(chainId) {
+    "1" -> "Mainnet"
+    "5" -> "Görli"
+    "10" -> "Optimism"
+    "137" -> "Polygon"
+    "8453" -> "Base"
+    "42161" -> "Arbitrum"
+    else -> "Loading..."
+}
 
-/*@Composable
+@SuppressLint("ServiceCast")
+private fun copyTextToClipboard(context: Context, text: String) {
+    val clipboardManager = context.getSystemService(Context.CLIPBOARD_SERVICE) as android.content.ClipboardManager
+    clipboardManager.setText(AnnotatedString(text))
+}
+
+
+private fun truncateText(text: String): String {
+    if (text.length > 19) {
+        return text.substring(0, 5) + "..." //+ text.takeLast(3) + " "
+    }
+    return text
+}
+
+
+@Composable
 @Preview
 fun ethOSNetworkPillPreview() {
-    ethOSTheme() {
-        ethOSNetworkPill("0xefBABdeE59968641DC6E892e30C470c2b40157Cd",1)
-    }
-}*/
+    ethOSNetworkPill(
+
+    address= "0xfebo235b5kcten3452b45b4o",
+    network= "Mainnet",
+    chainColor= Colors.SUCCESS,
+    )
+}
---
+++ library/src/main/java/org/ethosmobile/components/library/lightnode/InfoBlock.kt
@@ -0,0 +1,111 @@
+package org.ethosmobile.components.library.lightnode
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.R
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Info
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import org.ethosmobile.components.library.theme.Colors
+import org.ethosmobile.components.library.theme.Font
+
+@Composable
+fun InfoBlock(
+    modifier: Modifier = Modifier,
+    text: String,
+    title: String = "",
+    hasTitle: Boolean,
+    icon: @Composable () -> Unit,
+) {
+    Column (
+        verticalArrangement = Arrangement.spacedBy(12.dp),
+        modifier = modifier
+            .fillMaxWidth()
+    ){
+        if(hasTitle){
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                Text(
+                    //modifier = modifier.padding(start = 16.dp),
+                    text = title,
+                    fontSize = 16.sp,
+                    color = Colors.WHITE
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                icon()
+            }
+        }
+
+        Column (
+            modifier = modifier
+                .fillMaxWidth()
+                .clip(RoundedCornerShape(12.dp))
+                .background(Colors.TRANSPARENT)
+        ){
+            Text(
+                text = text,
+                color = Colors.WHITE,
+                lineHeight = 109.sp,
+                style = TextStyle(
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.SemiBold,
+                    fontFamily = Font.INTER
+                ),
+                modifier = Modifier.padding(vertical = 16.dp)
+            )
+
+        }
+    }
+
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewTinystatus(
+) {
+    InfoBlock(
+        text = "Ethereum mainnet",
+        title = "Network",
+        hasTitle = true,
+        icon = {
+            IconButton(
+                modifier = Modifier.size(16.dp),
+                onClick = {}//}
+            ) {
+                Icon(
+                    imageVector = Icons.Outlined.Info,
+                    contentDescription = "Information",
+                    tint = Color(0xFF9FA2A5),
+                    modifier = Modifier
+                        .clip(CircleShape)
+                    //.background(Color.Red)
+                )
+            }
+        }
+    )
+}
