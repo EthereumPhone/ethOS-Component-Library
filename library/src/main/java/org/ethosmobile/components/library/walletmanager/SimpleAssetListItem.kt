f8a844b14ce2d65aa2a5780284847edd6f6f9cd6
emunsi <emunsi123@outlook.com>
2024-01-02 19:26:08 +0100

    update simpleassetlistitem

--- library/src/main/java/org/ethosmobile/components/library/walletmanager/SimpleAssetListItem.kt
+++ library/src/main/java/org/ethosmobile/components/library/walletmanager/SimpleAssetListItem.kt
@@ -9,13 +9,14 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import org.ethosmobile.components.library.theme.Fonts
 
 @Composable
 fun ethOSSimpleAssetListItem(
     title: String,
-    value: Double,
+    value: String,
     fiatAmount: String = "\$TODO",
 ) {
 
@@ -24,7 +25,13 @@ fun ethOSSimpleAssetListItem(
         horizontalArrangement = Arrangement.SpaceBetween
     ) {
         Text(text = title.uppercase(), fontFamily = Fonts.INTER, fontWeight = FontWeight.SemiBold, fontSize = 18.sp, modifier = Modifier.weight(0.4f), color = Color.White)
-        Text(text = "${value}",fontFamily = Fonts.INTER, fontWeight = FontWeight.Medium, fontSize = 18.sp, textAlign = TextAlign.End, modifier = Modifier.weight(0.30f), color = Color.White )
+        Text(text = "${value.replace(",", ".").toDouble()}",fontFamily = Fonts.INTER, fontWeight = FontWeight.Medium, fontSize = 18.sp, textAlign = TextAlign.End, modifier = Modifier.weight(0.30f), color = Color.White )
         //Text(text = fiatAmount,fontFamily = Fonts.INTER, fontWeight = FontWeight.Medium, fontSize = 18.sp, textAlign = TextAlign.End, modifier = Modifier.weight(0.30f), color = Color(0xFF9FA2A5))
     }
+}
+
+@Preview
+@Composable
+fun ethOSSimpleAssetListItemPreview(){
+    ethOSSimpleAssetListItem("Elie","0,1523")
 }
