1f77fbf740d8a761d02a291fbed8b6d7a3e9a742
emunsi <emunsi123@outlook.com>
2024-03-16 19:46:01 +0100

    logo class implementation

---
+++ library/src/main/java/org/ethosmobile/components/library/theme/Logo.kt
@@ -0,0 +1,14 @@
+package org.ethosmobile.components.library.theme
+
+import org.ethosmobile.components.library.R
+
+class Logo {
+    companion object {
+        val ETHEREUM_LOGO = R.drawable.ethereum_logo
+        val OPTIMISM_LOGO = R.drawable.optimism_logo
+        val ARBITRUM_LOGO = R.drawable.arbitrum_logo
+        val BASE_LOGO = R.drawable.base_logo
+        val GOERLI_LOGO = R.drawable.goerli_logo
+        val POLYGON_LOGO = R.drawable.polygon_logo
+    }
+}
